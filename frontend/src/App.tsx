5528398d28bdb68195586c2f50e94b8f5a7dffc5
FocoFuturo <jj.focofuturo@gmail.com>
2022-07-17 18:53:08 -0300

    useState

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import Header from "./components/Header"
-import NotificationButton from "./components/NotificationButton"
-import SalesCard from "./components/SalesCard"
+import Header from "./components/Header";
+import NotificationButton from "./components/NotificationButton";
+import SalesCard from "./components/SalesCard";
 
 function App() {
   
@@ -23,4 +23,4 @@ function App() {
 
 }
 
-export default App
+export default App;
